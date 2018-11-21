d116bbfb39abdca73cc975982c745ee844b886ea
Misagh Rasooli <misaghrasooli@gmail.com>
2018-11-21 11:08:41 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
 <?php
-        echo "<h1>Hey world! : ",$_SERVER['SERVER_ADDR'],"</h1>";
+$conn = mysqli_connect("mysql", "trumpet", "rumpet123", "trumpet");
+
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+
+
+$sql = "CREATE TABLE test (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$sql = "SELECT * FROM test";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
 ?>
